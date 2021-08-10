2533c28edfcaec12ce6a074baac562c349a90d31
Ketanaa <86412087+Ketanaa@users.noreply.github.com>
2021-08-10 15:59:04 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,18 +14,15 @@ function preload () {
   scene = loadImage("Astronaut's daily routine/iss.png");
   sleep= loadAnimation("Astronaut's daily routine/sleep.png");
   brush= loadAnimation("Astronaut's daily routine/brush.png");
-  gym1= loadAnimation("Astronaut's daily routine/gym1.png","gym2.png","gym11.png","gym12.png");
-  eat= loadAnimation("Astronaut's daily routine/eat1.png","eat2.png");
-  drink= loadAnimation("Astronaut's daily routine/drink1.png","drink2.png");
-  bath= loadAnimation("Astronaut's daily routine/bath1.png","bath2.png");
-  move= loadAnimation("Astronaut's daily routine/move.png","move1.png");  
+  gym= loadAnimation("Astronaut's daily routine/gym1.png","Astronaut's daily routine/gym2.png","Astronaut's daily routine/gym11.png","Astronaut's daily routine/gym12.png");
+  eat= loadAnimation("Astronaut's daily routine/eat1.png","Astronaut's daily routine/eat2.png");
+  drink= loadAnimation("Astronaut's daily routine/drink1.png","Astronaut's daily routine/drink2.png");
+  bath= loadAnimation("Astronaut's daily routine/bath1.png","Astronaut's daily routine/bath2.png");
+  move= loadAnimation("Astronaut's daily routine/move.png","Astronaut's daily routine/move1.png");  
 }
 
 function setup() {
   createCanvas(800,400);
-
-  bg=createSprite(400, 200, 50, 50);
-  bg.addImage(backgroundImage);
 
   astronaut=createSprite(400, 200, 50, 50);
   astronaut.addAnimation("walking",move);
@@ -33,14 +30,17 @@ function setup() {
 }
 
 function draw() {
-  background(255,255,255); 
+  background(scene); 
   
+  fill("white");
+  textSize(20);
   text("Instructions:",0,0);
   text("Up Arrow = Brushing",0,15);
   text("Down Arrow = Gymming",0,30);
   text("Left Arrow = Eating",0,45);
   text("Right Arrow = Bathing",0,60);
   text("M key = Moving",0,75);
+
 
   if(keyDown("UP_ARROW")) {
     astronaut.addAnimation("brushing",brush);
@@ -75,8 +75,7 @@ function draw() {
   }
 
   if(keyDown("m")) {
-    astronaut.addAnimation("moving",move);
-    astronaut.changeAnimation("moving");
+    astronaut.changeAnimation("walking");
     astronaut.y=350;
     astronaut.velocity.X = -5;
     astronaut.velocity.Y = 5;
